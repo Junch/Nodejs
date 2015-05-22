64f217ac5466290aa56bd16cb4d07409700fdc1c
Jun Chen <junc76@gmail.com>
2015-05-22 15:50:41 +0800

    Apply the controller and filtering the products

---
+++ sports_store/angularjs/controllers/productListControllers.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("sportsStore")
+    .controller("productListCtrl", function ($scope, $filter) {
+
+        var selectedCategory = null;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null ||
+                product.category == selectedCategory;
+        }
+    });
+
+
