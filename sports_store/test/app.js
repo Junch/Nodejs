e246ec0b168882cf236ca8e1dd16cd95b8b661f0
Jun Chen <junc76@gmail.com>
2015-07-07 16:19:54 +0800

    sports_store: demonstration: use karma, jasmine to write a unit test.

---
+++ sports_store/test/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// The example is from https://docs.angularjs.org/guide/unit-testing
+
+angular.module('app', [])
+.controller('PasswordController', function PasswordController($scope) {
+  $scope.password = '';
+  $scope.grade = function() {
+    var size = $scope.password.length;
+    if (size > 8) {
+      $scope.strength = 'strong';
+    } else if (size > 3) {
+      $scope.strength = 'medium';
+    } else {
+      $scope.strength = 'weak';
+    }
+  };
+});
