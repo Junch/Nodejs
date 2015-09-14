e8dbcce67fc163e3894a018e9194d3bc79da67f7
Jun Chen <junc76@gmail.com>
2015-09-14 18:36:21 +0800

    Replace callback with promise in wines.js

--- wines/routes/wines.js
+++ wines/routes/wines.js
@@ -6,7 +6,7 @@ var MongoClient = Promise.promisifyAll(MongoDB.MongoClient);
 var ObjectID = MongoDB.ObjectID;
 var db;
 
-exports.connect = function (next) {
+exports.connect = (next) => {
     if (db == null){
         MongoClient.connectAsync('mongodb://localhost:27017/winedb').then((res) => {
             db = res;
@@ -17,7 +17,7 @@ exports.connect = function (next) {
     }
 };
 
-exports.findById = function (req, res) {
+exports.findById = (req, res) => {
     var idStr = req.params.id;
     console.log('Retrieving wine: ' + idStr);
 
@@ -30,29 +30,27 @@ exports.findById = function (req, res) {
         return;
     }
 
-    db.collection('wines').findOne({_id: id}, function (err, item) {
-        if (err) {throw err; }
+    db.collection('wines').findOneAsync({_id: id}).then((item) => {
         res.send(item);
     });
 };
 
 exports.findAll = function (req, res) {
-    db.collection('wines').find().toArray(function (err, items) {
-        if (err) {throw err; }
+    db.collection('wines').find().toArrayAsync().then((items) => {
         res.send(items);
+    }).catch((err) => {
+        res.send({error: 'An error has occurred - ' + err});
     });
 };
 
 exports.addWine = function (req, res) {
     var wine = req.body;
     console.log('Adding wine: ' + JSON.stringify(wine));
-    db.collection('wines').insert(wine, function (err, items) {
-        if (err) {
-            res.send({error: 'An error has occurred'});
-        } else {
-            console.log('Success: ' + JSON.stringify(items[0]));
-            res.send(items[0]);
-        }
+    db.collection('wines').insertAsync(wine).then((items) => {
+       console.log('Success: ' + JSON.stringify(items[0]));
+       res.send(items[0]);
+    }).catch((err) => {
+        res.send({error: 'An error has occurred'});
     });
 };
 
@@ -64,26 +62,22 @@ exports.updateWine = function (req, res) {
     if (wine._id) {
         delete wine._id;
     }
-    db.collection('wines').update({_id: new ObjectID(id)}, wine, function (err, result) {
-        if (err) {
-            console.log('Error updating wine: ' + err);
-            res.send({error: 'An error has occurred'});
-        } else {
-            console.log(String(result) + ' document(s) updated');
-            res.send(wine);
-        }
+    db.collection('wines').updateAsync({_id: new ObjectID(id)}, wine).then((result) => {
+        console.log(String(result) + ' document(s) updated');
+        res.send(wine);
+    }).catch((err) => {
+        console.log('Error updating wine: ' + err);
+        res.send({error: 'An error has occurred'});
     });
 };
 
 exports.deleteWine = function (req, res) {
     var id = req.params.id;
     console.log('Deleting wine: ' + id);
-    db.collection('wines').remove({_id: new ObjectID(id)}, function (err, result) {
-        if (err) {
-            res.send({error: 'An error has occurred - ' + err});
-        } else {
-            console.log(String(result) + ' document(s) deleted');
-            res.send(req.body);
-        }
+    db.collection('wines').removeAsync({_id: new ObjectID(id)}).then((result) => {
+        console.log(String(result) + ' document(s) deleted');
+        res.send(req.body);
+    }).catch((err) => {
+        res.send({error: 'An error has occurred - ' + err});
     });
 };
