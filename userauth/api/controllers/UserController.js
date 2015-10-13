7dccb6d9b21bb2a1b294d584d8edeb906448f52c
Jun - Home <junc76@gmail.com>
2015-10-13 22:26:39 +0800

    userauth: Save the encrypted password after signup

--- userauth/api/controllers/UserController.js
+++ userauth/api/controllers/UserController.js
@@ -8,6 +8,37 @@
 module.exports = {
 	signup: function(req, res){
 		console.log('Backend Signup');
+
+		var Passwords = require('machinepack-passwords');
+
+		Passwords.encryptPassword({
+			password: req.param('password'),
+			difficulty: 10
+		}).exec({
+			error: function(err){
+				return res.negotiate(err);
+			},
+			success: function(encryptPassword){
+				// Create User
+				User.create({
+					name: req.param('name'),
+					email: req.param('email'),
+					password: encryptPassword,
+					lastLoggedIn: new Date()
+				}, function userCreated(err, newUser){
+					if (err) {
+						console.log('Error:'+err);
+						return res.negotiate(err);
+					}
+
+					console.log('User Added');
+					return res.json({
+						id: newUser.id
+					});
+				})
+			}
+		})
+
 	}
 };
 
