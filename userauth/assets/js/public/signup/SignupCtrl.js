fc7682d29159648ea69d6207c962f6e01aa8256e
Jun - Home <junc76@gmail.com>
2015-10-13 21:53:25 +0800

    userauth: post user signup information to backend

--- userauth/assets/js/public/signup/SignupCtrl.js
+++ userauth/assets/js/public/signup/SignupCtrl.js
@@ -1,7 +1,17 @@
-angular.module('SignupMod').controller('SignupCtrl', ['$scope', function($scope){
+angular.module('SignupMod').controller('SignupCtrl', ['$scope', '$http', function($scope, $http){
 	console.log('Signup Controller initialized...');
 	
     $scope.runSignup = function() {
     	console.log('Signing up ' + $scope.name);
+
+    	$http.post('/signup', {
+    		name: $scope.name,
+    		email: $scope.email,
+    		password: $scope.password
+    	}).then(function success(res){
+    		window.location = '/user';
+    	}).catch(function error(err){
+    		console.log('Error' + err);
+    	});
     }
 }]);
