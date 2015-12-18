092bdca37b0263f46084272522f290f9b0ee905e
Jun Chen <junc76@gmail.com>
2015-12-18 11:16:36 +0800

    react_study: The React lifecycle methods getDefaultProps and getInitialState are replaced. http://www.toptal.com/react/navigating-the-react-ecosystem

--- react_study/hello.jsx
+++ react_study/hello.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class Hello extends React.Component {
+	static defaultProps = { initialCount: 0}
+	static propTypes = {initialCount: React.PropTypes.number}
+
+	constructor(props) {
+		super(props);
+		this.state = {count: props.initialCount};
+	}
+
+	static propTypes = {
+		count: React.PropTypes.number.isRequired
+	}
+
+	tick() {
+		this.setState({count: this.state.count + 1});
+	}
+
 	render() {
-		return <h1>Hello</h1>
+		return <h1 onClick={this.tick.bind(this)}>Hello: {this.state.count}</h1>
 	}
 
 	componentWillMount() {
