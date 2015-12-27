9313045b4accf3bc454c3ce3b3ec4df6debb7ca5
Jun Chen <junc76@gmail.com>
2015-12-27 23:40:13 +0800

    react: Optimizing webpack product build. Refer to link [Optimizing Webpack Production Build for React + ES6 Apps](https://medium.com/modus-create-front-end-development/optimizing-webpack-production-build-for-react-es6-apps-a637e5692aea#.m3zo55uq5)

--- react_tutorial/webpack.config.js
+++ react_tutorial/webpack.config.js
@@ -2,11 +2,19 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+	devtool: 'cheap-module-source-map',
 	entry: [
 		'babel-polyfill',
 		'./main.js'
 		],
 	output: {path: __dirname, filename: 'bundle.js'},
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
 	module: {
 		loaders: [
 			{
