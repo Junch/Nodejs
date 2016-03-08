1b08b59436e13967a0c990c0543cce52667a1e83
Jun Chen <junc76@gmail.com>
2016-03-08 23:34:41 +0800

    react_stock: Send the sell/buy information to the backend (not complete).

--- react_stock/static/js/formTrade.jsx
+++ react_stock/static/js/formTrade.jsx
@@ -6,22 +6,38 @@ import moment from 'moment';
 export default class FormTrade extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = {startDate: moment()};
+    this.state = {startDate: moment(), price:'', volume: ''};
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    axios.post('/api/trade', {
+      symbol: 'sh600005',
+      volume: Number(this.state.volume),
+      date: this.state.startDate,
+      price: Number(this.state.price) 
+    }).then(function(response){
+      this.setState({price: '', volume: ''});
+    }.bind(this)).catch(function(response){
+      this.setState({price: '', volume: ''});
+      console.log(response);
+    }.bind(this));
   }
 
   render() {
     return (
-      <div className="col-sm-12" style={{marginBottom: "15px"}}>
+      <div className="col-sm-12" style={{marginBottom: "1.2em"}}>
         <div className="col-sm-6" >
           <Button bsStyle="primary" onClick={ ()=> this.setState({ open: !this.state.open })}>
             买卖
           </Button>
           <Collapse in={this.state.open}>
-            <form style={{marginTop: "15px"}}>
+            <form style={{marginTop: "1.2em"}} onSubmit={this.handleSubmit.bind(this)}>
               <div className="form-group row">
                 <label htmlFor="type" className="col-sm-2 form-control-label">类型</label>
                 <div className="col-sm-10">
-                  <select type="email" className="form-control" id="type">
+                  <select className="form-control" id="type">
                     <option value="1">买入</option>
                     <option value="1">卖出</option>
                   </select>
@@ -30,19 +46,19 @@ export default class FormTrade extends React.Component {
               <div className="form-group row">
                 <label htmlFor="date" className="col-sm-2 form-control-label">日期</label>
                 <div className="col-sm-10">
-                  <DatePicker selected={this.state.startDate} onChange={ (date)=> this.setState({startDate: date}) }/>
+                  <DatePicker selected={this.state.startDate} onChange={ (date)=> this.setState({startDate: date})} />
                 </div>
               </div>
               <div className="form-group row">
                 <label htmlFor="price" className="col-sm-2 form-control-label">价格</label>
                 <div className="col-sm-10">
-                  <input type="email" className="form-control" id="price"/>
+                  <input type="number" className="form-control" id="price" min="0" step="0.001" value={this.state.price} onChange={(e)=> this.setState({price: e.target.value})} />
                 </div>
               </div>
               <div className="form-group row">
                 <label htmlFor="volume" className="col-sm-2 form-control-label">数量</label>
                 <div className="col-sm-10">
-                  <input type="email" className="form-control" id="volume"/>
+                  <input type="number" className="form-control" id="volume" min="0" step="100" value={this.state.volume} onChange={(e)=> this.setState({volume: e.target.value})} />
                 </div>
               </div>
               <Button type="submit" bsStyle="primary">Submit</Button>
