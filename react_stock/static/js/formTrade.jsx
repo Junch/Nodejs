d61c5aa47d70946471fdcaa7e0a87ab6a6916a75
Jun Chen <junc76@gmail.com>
2016-03-10 00:17:56 +0800

    react_stock: Use Alert to give feedback to user after buying or selling stocks

--- react_stock/static/js/formTrade.jsx
+++ react_stock/static/js/formTrade.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Collapse, Button} from 'react-bootstrap';
+import { Collapse, Button, Alert} from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
 export default class FormTrade extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = {startDate: moment(), symbol: '', price:'', volume: ''};
+    this.state = {startDate: moment(), symbol: '', price:'', volume: '', alertVisible: false, alertMsg: '', alertStyle: 'success'};
   }
 
   handleSubmit(e) {
@@ -18,14 +18,25 @@ export default class FormTrade extends React.Component {
       date: this.state.startDate,
       price: Number(this.state.price) 
     }).then(function(response){
-      this.setState({symbol:'', price: '', volume: ''});
+      let msg = `交易成功：买入股票${this.state.symbol},价格${this.state.price}元,数量${this.state.volume}`
+      this.setState({symbol:'', price: '', volume: '', alertMsg: msg, alertVisible: true, alertStyle: "success"});
     }.bind(this)).catch(function(response){
-      this.setState({price: '', volume: ''});
+      let msg = `交易失败：买入股票${this.state.symbol},价格${this.state.price}元,数量${this.state.volume}`
+      this.setState({symbol:'', price: '', volume: '', alertMsg: msg, alertVisible: true, alertStyle: "danger"});
       console.log(response);
     }.bind(this));
   }
 
   render() {
+    let alertDiv = <div/>
+    if (this.state.alertVisible) {
+      alertDiv = (
+        <Alert bsStyle={this.state.alertStyle} style={{marginTop: "1.2em"}} onDismiss={()=> this.setState({alertVisible: false})} dismissAfter={5000} >
+          {this.state.alertMsg}
+        </Alert>
+      )
+    }
+
     return (
       <div className="col-sm-12" style={{marginBottom: "1.2em"}}>
         <div className="col-sm-6" >
@@ -52,22 +63,23 @@ export default class FormTrade extends React.Component {
               <div className="form-group row">
                 <label htmlFor="date" className="col-sm-2 form-control-label">日期</label>
                 <div className="col-sm-10">
-                  <DatePicker selected={this.state.startDate} onChange={ (date)=> this.setState({startDate: date})} />
+                  <DatePicker selected={this.state.startDate} onChange={ date=> this.setState({startDate: date})} />
                 </div>
               </div>
               <div className="form-group row">
                 <label htmlFor="price" className="col-sm-2 form-control-label">价格</label>
                 <div className="col-sm-10">
-                  <input type="number" className="form-control" id="price" min="0" step="0.001" value={this.state.price} onChange={(e)=> this.setState({price: e.target.value})} />
+                  <input type="number" className="form-control" id="price" min="0" step="0.001" value={this.state.price} onChange={ e=> this.setState({price: e.target.value})} />
                 </div>
               </div>
               <div className="form-group row">
                 <label htmlFor="volume" className="col-sm-2 form-control-label">数量</label>
                 <div className="col-sm-10">
-                  <input type="number" className="form-control" id="volume" min="0" step="100" value={this.state.volume} onChange={(e)=> this.setState({volume: e.target.value})} />
+                  <input type="number" className="form-control" id="volume" min="0" step="100" value={this.state.volume} onChange={ e=> this.setState({volume: e.target.value})} />
                 </div>
               </div>
               <Button type="submit" bsStyle="primary">Submit</Button>
+              {alertDiv}
             </form>
           </Collapse>
         </div>
