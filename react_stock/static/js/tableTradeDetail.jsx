0ac09c8a7a2b87f8763e4f1edac8706d17582826
Jun Chen <junc76@gmail.com>
2016-04-03 17:28:56 +0800

    react_stock: refactor: rename some variables

--- react_stock/static/js/tableTradeDetail.jsx
+++ react_stock/static/js/tableTradeDetail.jsx
@@ -8,16 +8,16 @@ import {formatPercent} from './util.jsx'
 class TableTradeDetail extends React.Component {
   constructor(props){
     super(props);
-    this.state = {showModal: false}
+    this.state = {showModalConfirm: false}
   }
 
-  open(trade) {
+  openModalConfirm(trade) {
     this.trade = trade;
-    this.setState({ showModal: true });
+    this.setState({ showModalConfirm: true });
   }
 
-  close(confirm) {
-    this.setState({ showModal: false });
+  closeModalConfirm(confirm) {
+    this.setState({ showModalConfirm: false });
     if (this.trade != null && confirm) {
       this.props.onDeleteTrade(this.trade);
     }
@@ -35,8 +35,8 @@ class TableTradeDetail extends React.Component {
           <td style={{textAlign: "right"}}>{trade.volume}</td>
           <td style={{textAlign: "right"}}>
             <a><span className="glyphicon glyphicon-edit"/></a>&nbsp;
-            <a onClick={this.open.bind(this, trade)}><span className="glyphicon glyphicon-trash"/></a>
-            <Modal show={this.state.showModal} bsSize="small" container={this} onHide={e => this.close(false)}>
+            <a onClick={this.openModalConfirm.bind(this, trade)}><span className="glyphicon glyphicon-trash"/></a>
+            <Modal show={this.state.showModalConfirm} bsSize="small" container={this} onHide={e => this.close(false)}>
               <Modal.Header>
                 <Modal.Title>提醒</Modal.Title>
               </Modal.Header>
@@ -44,8 +44,8 @@ class TableTradeDetail extends React.Component {
                 确定删除这个交易?
               </Modal.Body>
               <Modal.Footer>
-                <Button bsSize="small" bsStyle="primary" onClick={e => this.close(true)}>确定</Button>
-                <Button bsSize="small" onClick={e => this.close(false)}>取消</Button>
+                <Button bsSize="small" bsStyle="primary" onClick={e => this.closeModalConfirm(true)}>确定</Button>
+                <Button bsSize="small" onClick={e => this.closeModalConfirm(false)}>取消</Button>
               </Modal.Footer>
             </Modal>
           </td>
