949c52eb9689a7a5e009238f1b89e86767c5f672
Jun Chen <junc76@gmail.com>
2016-04-04 11:13:35 +0800

    react_stock: Show confirm dialog when deleting a cash trade

--- react_stock/static/js/tableCash.jsx
+++ react_stock/static/js/tableCash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Table, Button, Modal } from 'react-bootstrap';
+import ModalConfirm from './modalConfirm.jsx'
 import '../css/zui-table.css'
 import '../css/modal-dialog.css'
 import accounting from 'accounting';
@@ -8,6 +9,21 @@ import {formatPercent} from './util.jsx'
 class TableCash extends React.Component {
   constructor(props){
     super(props);
+    this.state = {showModalConfirm: false}
+  }
+
+  openModalConfirm(cash) {
+    this.cash = cash;
+    this.setState({ showModalConfirm: true });
+  }
+
+  closeModalConfirm(confirm) {
+    this.setState({ showModalConfirm: false });
+    if (this.cash != null && confirm) {
+      this.props.onDeleteCash(this.cash);
+    }
+
+    this.cash = null
   }
 
   render() {
@@ -17,13 +33,16 @@ class TableCash extends React.Component {
           <td>{index}</td>
           <td>{(new Date(cash.date)).toLocaleDateString()}</td>
           <td style={{textAlign: "right"}}>{accounting.formatMoney(cash.volume)}</td>
-          <td className="col-md-2" style={{textAlign: "right"}}><a><span className="glyphicon glyphicon-edit"/></a>&nbsp;<a onClick={this.props.onDeleteCash.bind(this, cash)}><span className="glyphicon glyphicon-trash"/></a></td>
+          <td className="col-md-2" style={{textAlign: "right"}}><a><span className="glyphicon glyphicon-edit"/></a>&nbsp;
+          <a onClick={this.openModalConfirm.bind(this, cash)}><span className="glyphicon glyphicon-trash"/></a>
+          <ModalConfirm showModal={this.state.showModalConfirm} closeModal={this.closeModalConfirm.bind(this)} container={this} />
+          </td>
         </tr>
       );
     });
 
     return (
-      <div className="row">
+      <div className="row modal-container">
         <h3>本金</h3>
         <div className="col-md-6" style={{paddingLeft: 0}}>
           <table className="zui-table zui-table-rounded">
