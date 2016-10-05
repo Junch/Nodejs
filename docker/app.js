22ae9fe8cf27a29ff6eef0780e92f52b31bafcf2
Jun Chen <junc76@gmail.com>
2016-10-05 14:30:46 +0800

    docker: nodejs connect to mongo database using .yml file and the environment variable

--- docker/app.js
+++ docker/app.js
@@ -1,7 +1,38 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+var db;
 
+MongoClient.connect(url, function (err, database) {
+    if (err) {
+        console.log("Failed to connect to server");
+    } else {
+        console.log("Connected to server successfully");
+        db = database;
+    }
+});
+
+app.use(bodyparser.json());
 app.use(express.static('public'));
+
+var insertDocument = function (db, document, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+
+    // Insert some documents
+    collection.insertOne(document, function (err, result) {
+        callback(err, JSON.stringify(result.ops[0]));
+    });
+};
+
+app.post('/hello', function (req, res) {
+    var data = req.body;
+    insertDocument(db, data, function(err, result) {
+        res.status(201).send(result)
+    })
+});
 
 app.get('/hello', function (req, res) {
     res.send('world');
