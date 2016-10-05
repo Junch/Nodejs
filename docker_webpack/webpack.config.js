0cb3805b756bc89189768b0757ca9bb083166dc2
Jun Chen <junc76@gmail.com>
2016-10-06 01:36:27 +0800

    docker_webpack: dockerize the webpack but without supporting nodejs in this example

---
+++ docker_webpack/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './public/main.jsx',
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+      { test: /\.js[x]?$/,
+        include: path.join(__dirname, 'public'),
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+      { test: /\.css$/,
+        loader: 'style-loader!css-loader?modules' 
+      }
+    ]
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './public',
+    host: '0.0.0.0'
+  }
+};
