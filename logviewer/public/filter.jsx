9fb0f8aa4a65f7c600b1193a9a2acbb967ec214c
Jun Chen <junc76@gmail.com>
2016-11-01 23:33:53 +0800

    logviewer: bootstrap slider: Use moment to replace Date

--- logviewer/public/filter.jsx
+++ logviewer/public/filter.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+let moment = require('moment');
 
 export default class FilterPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {start: Date.parse("Wed, 09 Aug 1995 08:00:00"),
-                  end: Date.parse("Wed, 09 Aug 1995 11:00:00")};
+    this.state = {start: moment("2016-10-11 15:17:50,990", moment.ISO_8601).valueOf(),
+                    end: moment("2016-10-11 18:04:55,998", moment.ISO_8601).valueOf()};
   }
 
   componentDidMount() {
@@ -12,9 +13,9 @@ export default class FilterPage extends React.Component {
       formatter: function(value) {
         if (Array.isArray(value)) {
           let ranges = value.map(item => {
-            return new Date(item).toString();
+            return moment(item).format();
           });
-          return ranges.join(',');
+          return ranges.join(', ');
         } else{
           return value;
         }
@@ -31,7 +32,7 @@ export default class FilterPage extends React.Component {
             <label htmlFor="ex2" className="col-sm-2 control-label">Span</label>
             <div className="col-sm-10">
               <div>
-                <input id="ex2" type="text" className="span2" value="" data-slider-min={this.state.start} data-slider-tooltip="show" data-slider-max={this.state.end} data-slider-step="5" style={{width: "80%"}} data-slider-value="[807926400000, 807937200000]" />
+                <input id="ex2" type="text" className="span2" value="" data-slider-min={this.state.start} data-slider-tooltip="show" data-slider-max={this.state.end} data-slider-step="5" style={{width: "80%"}} data-slider-value="[1476170270990, 1476180295998]" />
               </div>
             </div>
           </div>
