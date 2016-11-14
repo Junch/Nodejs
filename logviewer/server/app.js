a245b427c91583cf1c97ed636cf3ab29e32bafff
Jun Chen <junc76@gmail.com>
2016-11-14 16:08:27 +0800

    logviewer: Add the log information at backend

--- logviewer/server/app.js
+++ logviewer/server/app.js
@@ -1,9 +1,13 @@
 let path = require('path');
 let express = require('express');
-let compression = require('compression')
+let compression = require('compression');
+let morgan = require('morgan');
+let logger = require('./logger');
 
 let app = express();
 app.use(compression());
+logger.debug('Overriding \'Express\' logger');
+app.use(morgan('combined', {stream: logger.stream}));
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 let port = process.env.PORT || 9090;
---
+++ logviewer/server/logger.js
@@ -0,0 +1,36 @@
+let winston = require('winston');
+winston.emitErrs = true;
+
+// http://stackoverflow.com/questions/13157330/logging-with-winston-in-express-js-how-to-configure-for-different-environments
+let logTransports = [];
+if (process.env.NODE_ENV == 'production') {
+    logTransports.push(new (winston.transports.File)({
+        level: 'info',
+        filename: './logs/all-logs.log',
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false
+    }));
+} else {
+    logTransports.push(new (winston.transports.Console)({
+        level: 'debug', // error
+        handleExceptions: true,
+        json: false,
+        colorize: true
+    }));
+}
+
+let logger = new winston.Logger({
+    transports: logTransports,
+    exitOnError: false
+});
+
+logger.stream =  {
+    write: function (message, encoding) {
+        logger.info(message);
+    }
+};
+
+module.exports = logger;
