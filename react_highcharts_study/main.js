3499a6d920b7ada408034c3bc0a27d4e832498da
Jun Chen <junc76@gmail.com>
2018-01-07 02:43:33 +0800

    react_highcharts_study: Use pure highchairs API to draw the chart

--- react_highcharts_study/main.js
+++ react_highcharts_study/main.js
@@ -5,3 +5,67 @@ import World from './world.jsx';
 import MyChart from './chart.jsx';
 
 render( <MyChart />, document.getElementById('chart'));
+
+import Highcharts from 'highcharts'
+import addMore from "highcharts/highcharts-more";
+import addDrilldown from "highcharts/modules/drilldown";
+import addNoData from "highcharts/modules/no-data-to-display";
+import addExporting from "highcharts/modules/exporting";
+addMore(Highcharts)
+addDrilldown(Highcharts)
+addNoData(Highcharts)
+addExporting(Highcharts)
+
+const chart = Highcharts.chart('pie', {
+    // https://www.highcharts.com/demo/pie-basic/
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares January, 2015 to May, 2015'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'IE',
+            y: 56.33
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Firefox',
+            y: 10.38
+        }, {
+            name: 'Safari',
+            y: 4.77
+        }, {
+            name: 'Opera',
+            y: 0.91
+        }, {
+            name: 'Other',
+            y: 0.2
+        }]
+    }]
+});
