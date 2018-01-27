dafd33edb07cde8a7caee48da1b2e51070a5be76
Jun Chen <junc76@gmail.com>
2018-01-27 16:27:10 +0800

    tushare: compare the method profit.compare_item to show more stocks

--- tushare/profit.py
+++ tushare/profit.py
@@ -24,38 +24,31 @@ def write_profit():
     result.to_csv('profit.csv', encoding='utf-8')
 
 def compare_item(df, column, stocks, title):
-    df1 = df.loc[(df.index.get_level_values('code') == stocks[0]) &
-                 (df.index.get_level_values('quarter').str.endswith('4'))]
-    df2 = df.loc[(df.index.get_level_values('code') == stocks[1]) &
-                 (df.index.get_level_values('quarter').str.endswith('4'))]
+    ss = []
+    labels = []
+    for stock in stocks:
+        dfstock = df.loc[(df.index.get_level_values('code') == stock) &
+                         (df.index.get_level_values('quarter').str.endswith('4'))]
+        ss.append(dfstock[column])
+        labels.append(dfstock.name[0])
 
-    s1 = df1[column]
-    s2 = df2[column]
-    label1 = df1.name[0]
-    label2 = df2.name[0]
-
-    n_groups = len(s1)
-    
     # create plot
     fig, ax = plt.subplots(figsize=(8,6))
-    index = np.arange(n_groups)
-    bar_width = 0.36
+    stocknum = len(ss)
+    groupnum = len(ss[0])
+    index = np.arange(groupnum)
+    bar_width = 1.0/(stocknum+1)
     opacity = 0.8
     
-    rects1 = plt.bar(index, s1, bar_width,
-                    alpha=opacity,
-                    color='b',
-                    label=unicode(label1, 'utf-8'))
-
-    rects2 = plt.bar(index + bar_width, s2, bar_width,
-                    alpha=opacity,
-                    color='g',
-                    label=unicode(label2, 'utf-8'))
+    for idx, s in enumerate(ss):
+        plt.bar(index + idx*bar_width, s, bar_width,
+                alpha=opacity,
+                label=unicode(labels[idx], 'utf-8'))
 
     plt.legend(prop={'family':'SimHei','size':15})
     plt.ylabel(column)
     plt.title(title)
-    plt.xticks(index+bar_width/2.0, s1.index.get_level_values('quarter'))
+    plt.xticks(index + (stocknum-1)*bar_width/2.0, ss[0].index.get_level_values('quarter'))
     plt.legend()
     plt.grid(linestyle='dotted')
     plt.tight_layout()
@@ -66,7 +59,9 @@ def read_profit():
     df.set_index(['quarter', 'code'], inplace=True)
     df.sort_index()
 
-    compare_item(df, 'net_profits', ['000651', '000333'], u'净利润比较')
+    compare_item(df, 'net_profits', ['000651', '000333', '600690', '002415'], u'净利润比较')
+    #compare_item(df, 'net_profits', ['000651', '000333'], u'净利润比较')
+    #compare_item(df, 'net_profits', ['000651'], u'净利润比较')
 
 if __name__=="__main__":
     #write_profit()
