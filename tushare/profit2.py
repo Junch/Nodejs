c985e405720a8c2d8729d1b7a0f5e83577f56fa8
Jun Chen <junc76@gmail.com>
2018-01-29 01:26:08 +0800

    tushare: compare profits in a period, and can specify quarter or year

--- tushare/profit2.py
+++ tushare/profit2.py
@@ -8,27 +8,34 @@ import time
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False   #用来正常显示负号
 
+g_start = 0
+
 def write_profit():
-    frames = []
     arr = [(x, y) for x in range(2007, 2018) for y in range (1, 5)]
     arr.remove((2017, 4))
 
+    frames = []
     for item in arr:
         df = ts.get_profit_data(item[0], item[1])
         df.set_index('code', inplace=True)
         frames.append(df)
 
-    quaters = ['{}-{}'.format(x,y) for (x, y) in arr]
+    quaters = [pd.Period('{}Q{}'.format(x,y)) for (x, y) in arr]
     result = pd.concat(frames, keys=quaters)
     result.index.names=[u'quarter', u'code']
     result.to_csv('profit.csv', encoding='utf-8')
 
-def compare_item(df, column, stocks, title):
+def compare_item(df, periods, column, stocks, title):
     ss = []
     labels = []
     for stock in stocks:
-        dfstock = df.loc[(df.index.get_level_values('code') == stock) &
-                         (df.index.get_level_values('quarter').str.endswith('4'))]
+        df1 = df.loc[df.code == stock]
+        df1.set_index('quarter', inplace=True)
+
+        c = (df1.index <= periods['end']) & (df1.index >= periods['start'])
+        if (periods['year']):
+            c = c & (df1.index.quarter == 4)
+        dfstock = df1.loc[c]
         ss.append(dfstock[column])
         labels.append(dfstock.name[0])
 
@@ -57,13 +64,14 @@ def compare_item(df, column, stocks, title):
     plt.show()
 
 def read_profit():
-    df = pd.read_csv('profit.csv', dtype={'code' : np.str})
-    df.set_index(['quarter', 'code'], inplace=True)
-    df.sort_index()
-
+    df = pd.read_csv('profit.csv', dtype={'code' : np.str}, parse_dates=['quarter'])
+    df.quarter = df.quarter.dt.to_period("Q") #df.quarter = pd.DatetimeIndex(df.quarter).to_period('Q')
+    #df.set_index(['quarter', 'code'], inplace=True) #speed will slow down dramatically
+    #df.sort_index()
+    periods = {'start':'2007Q1', 'end':'2017Q4', 'year': True}
     end = time.time()
     print(end - g_start)
-    compare_item(df, 'net_profits', ['000651', '000333', '600690', '002415'], '净利润比较')
+    compare_item(df, periods,'net_profits', ['000651', '000333', '600690', '002415'], '净利润比较')
 
 if __name__=="__main__":
     #write_profit()
