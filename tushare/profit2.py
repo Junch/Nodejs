168f0993a50cf6268939582302c2c43023958ad4
Jun Chen <junc76@gmail.com>
2018-01-30 16:42:09 +0800

    tushare: Use Pipfile to manager the dependencies.

--- tushare/profit2.py
+++ tushare/profit2.py
@@ -5,8 +5,8 @@ import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False   #用来正常显示负号
+# plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus']=False   #用来正常显示负号
 
 def CAGR(s):
     first = s.iloc[0]
